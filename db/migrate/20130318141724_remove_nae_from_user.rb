8f69ba955bd56320b18196ca0aed2fc191658ad9
Tavai Moors <tavaimoors@gmail.com>
2013-03-18 17:55:32 -0700

    Removed RemoveNaeFromUser table

--- db/migrate/20130318141724_remove_nae_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNaeFromUser < ActiveRecord::Migration
-  def up
-    remove_column :users, :nae
-  end
-
-  def down
-    add_column :users, :nae, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318141724) do
+ActiveRecord::Schema.define(:version => 20130317052846) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20130318141724) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "nae"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
