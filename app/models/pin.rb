d9cb51507b5762becd5412706e0107c92c8923db
Tavai Moors <tavaimoors@gmail.com>
2013-04-07 09:47:25 -0700

    Added Endless Scroll

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -10,7 +10,7 @@ class Pin < ActiveRecord::Base
 
 
   belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240>"}
+  has_attached_file :image, styles: { medium: "320x320>"}
 
 def image_remote_url=(url_value)
 	self.image = URI.parse(url_value) unless url_value.blank?
